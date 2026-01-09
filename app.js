f7ac049becec0e5cc85714edfd7ff1e16e2e0002
HostingerDeploy <deploy@hostinger.com>
2026-01-08 20:45:12 -0400

    Hostinger Debug Logging

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 
+process.on('uncaughtException', err => {
+  console.error('Uncaught exception:', err);
+});
+process.on('unhandledRejection', err => {
+  console.error('Unhandled rejection:', err);
+});
+
 const http = require('http');
 
-const port = process.env.PORT || 3000; // platform will override this
+const port = process.env.PORT || 3000;
 const host = '0.0.0.0';
+
+console.log('Starting server on', host, port);
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
