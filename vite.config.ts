a988ba6e00f3570ec7306514bb90666e79511560
HostingerDeploy <deploy@hostinger.com>
2026-01-10 03:00:19 -0400

    Reactivar plugin Hono server y SSR para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -38,14 +38,10 @@ export default defineConfig({
   plugins: [
     nextPublicProcessEnv(),
     restartEnvFileChange(),
-    ...(process.env.VERCEL
-      ? []
-      : [
-        reactRouterHonoServer({
-          serverEntryPoint: './__create/index.ts',
-          runtime: 'node',
-        }),
-      ]),
+    reactRouterHonoServer({
+      serverEntryPoint: './__create/index.ts',
+      runtime: 'node',
+    }),
     babel({
       include: ['src/**/*.{js,jsx,ts,tsx}'], // or RegExp: /src\/.*\.[tj]sx?$/
       exclude: /node_modules/, // skip everything else
